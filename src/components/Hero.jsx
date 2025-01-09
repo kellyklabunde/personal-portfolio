c249d0d3a9300d53766fd9e17d345a89195ffab5
unknown <kelly.klabunde@gmail.com>
2025-01-09 19:17:19 +0100

    initial commit, instalation and config

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+"use client";
+import Image from "next/image";
+
+const Hero = () => {
+    return <div className="h-screen grid place-items-center">
+        <div>
+            <div>
+                <div>
+                    <Image />
+                </div>
+            </div>
+        </div>
+    </div>
+   
+}
+
+export default Hero;
