2b9e9fdf277107d7f9c57d8cf57ddc38162ae017
unknown <kelly.klabunde@gmail.com>
2025-01-28 10:33:55 +0100

    contact session added

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import About from '../components/About';
 import Experience from '../components/Experience';
 import Skills from '../components/Skills';
 import Projects from '../components/Projects';
+import Contact from '@/components/Contact';
 import Navbar from '@/components/Navbar';
 import { useEffect, useRef, useState } from 'react';
 
@@ -38,6 +39,7 @@ export default function Home() {
         <Experience />
         <Skills />
         <Projects />
+        <Contact />
       </div>
     </>
   );
---
+++ src/components/Contact.jsx
@@ -0,0 +1,28 @@
+'use client';
+import Heading from "./sub/Heading";
+import Image from "next/image";
+import { motion } from "framer-motion";
+
+const Contact = () => {
+    return (
+        <div id="contact" className="h-screen py-20 px-96">
+            <Heading text={'Get in touch'}/>
+            <div className="w-full h-full my-auto flex lg:flex-col items-center justify-between lg:justify-center gap-x-20 lg:gap-x-0 gap-y-20">
+                <motion.div initial={{opacity: 0, y: 150}} whileInView={{opacity: 1, y: 0}} transition={{duration: .4}} viewport={{once: true}}>
+                    <Image src={'/contact.png'} alt="Contact Image" width={400} height={400} className="w-[400px] opacity-80" />
+                </motion.div>
+                <motion.form className="w-[600px] lg:w-[400px] sm:w-full flex flex-col gap-y-3" initial={{opacity: 0, x: 150}} whileInView={{opacity: 1, x: 0}} transition={{duration: .4}} viewport={{once: true}}>
+                    <div className="w-full flex lg:flex-col gap-x-3 lg:gap-y-3">
+                        <input type="text" placeholder="Your Name" className="w-full border border-yellow-500 rounded-md bg-zinc-100 px-4 py-2 text-sm tracking-wider text-gray-500 outline-none" />
+                        <input type="email" placeholder="Your Email" className="w-full border border-yellow-500 rounded-md bg-zinc-100 px-4 py-2 text-sm tracking-wider text-gray-500 outline-none"  />
+                    </div>
+                    <input type="text" placeholder="Subject" className="w-full border border-yellow-500 rounded-md bg-zinc-100 px-4 py-2 text-sm tracking-wider text-gray-500 outline-none" />
+                    <textarea placeholder="Write me a message" className="max-h-[250px] min-h-[150px] border border-yellow-500 rounded-md bg-zinc-100 px-4 py-2 text-sm tracking-wider text-gray-500 outline-none" />
+                    <button type="submit" className="w-full border border-yellow-500 rounded-md bg-yellow-600 px-4 py-2 text-sm font-light tracking-wider text-white outline-none hover:bg-yellow-500 transition-colors cursor-pointer">Send Message</button>
+                </motion.form>
+            </div>
+        </div>
+    );
+};
+
+export default Contact;
